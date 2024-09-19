2dc90c1653d7b6da61bc3b24b9860ae12c5e891d
Roberto Escobar <rescobar8@cuc.edu.co>
2024-09-19 09:52:52 -0500

    m1

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+import milogo from "./assets/img/logoReact.png"
+function App() {
+
+
+  return (
+    <>
+ <div>
+  <img src ={milogo} alt="Logo de React" />
+  <h1>Hola mundo</h1>
+  <p>Este es mi primer proyecto en read</p>
+
+ </div>
+    </>
+  )
+}
+
+export default App
